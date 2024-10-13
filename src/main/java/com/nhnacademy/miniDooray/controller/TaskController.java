478ef319c58c8ce9321ec241dfe9615f0565ebf5
yeon-so <nordstromm03@gmail.com>
2024-10-13 17:50:35 +0900

    feat: X-USER-header 추가

--- src/main/java/com/nhnacademy/miniDooray/controller/TaskController.java
+++ src/main/java/com/nhnacademy/miniDooray/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.miniDooray.dto.task.TaskDetailResponseDto;
 import com.nhnacademy.miniDooray.dto.task.TaskModifyRequestDto;
 import com.nhnacademy.miniDooray.dto.task.TaskRegisterRequestDto;
 import com.nhnacademy.miniDooray.dto.task.TaskResponseDto;
+import com.nhnacademy.miniDooray.service.CommonService;
 import com.nhnacademy.miniDooray.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,12 +20,16 @@ import java.util.List;
 public class TaskController {
 
     private final TaskService taskService;
+    private final CommonService commonService;
 
     @GetMapping("/{projectId}")
-    public List<TaskResponseDto> getAllTasksByProjectId(@PathVariable String projectId) {
+    public List<TaskResponseDto> getAllTasksByProjectId(
+            @PathVariable String projectId,
+            @RequestHeader(value = "X-USER-ID", required = true) String userId) {
 
         long id = parseId(projectId);
 
+        commonService.isUserMemberOfProject(id, userId);
         return taskService.getAllTasksByProjectId(id);
 
     }
@@ -32,19 +37,23 @@ public class TaskController {
     @GetMapping("/{projectId}/{taskId}")
     public TaskDetailResponseDto getTaskByProjectIdAndTaskId(
             @PathVariable String projectId,
-            @PathVariable String taskId){
+            @PathVariable String taskId,
+            @RequestHeader(value = "X-USER-ID", required = true) String userId){
 
         long projectIdLong = parseId(projectId);
         long taskIdLong = parseId(taskId);
 
+        commonService.isUserMemberOfProject(projectIdLong, userId);
         return taskService.getTaskByProjectIdAndTaskId(projectIdLong, taskIdLong);
 
     }
 
     @PostMapping("/{projectId}")
     public MessageResponseDto registerTask(@PathVariable String projectId,
-                                           @RequestBody TaskRegisterRequestDto requestDto){
-        long projectIdLong = parseId(projectId);
+                                           @RequestBody TaskRegisterRequestDto requestDto,
+                                           @RequestHeader(value = "X-USER-ID", required = true) String userId){
+       long projectIdLong = parseId(projectId);
+        commonService.isUserMemberOfProject(projectIdLong, userId);
         taskService.resgisterTask(projectIdLong, requestDto);
 
         return new MessageResponseDto(200, "OK");
@@ -55,10 +64,13 @@ public class TaskController {
     public MessageResponseDto getTaskByProjectIdAndTaskId(
             @RequestBody TaskModifyRequestDto requestDto,
             @PathVariable String projectId,
-            @PathVariable String taskId){
+            @PathVariable String taskId,
+            @RequestHeader(value = "X-USER-ID", required = true) String userId){
 
         long projectIdLong = parseId(projectId);
         long taskIdLong = parseId(taskId);
+
+        commonService.isUserMemberOfProject(projectIdLong, userId);
         taskService.modifyTask(projectIdLong, taskIdLong, requestDto);
 
         return new MessageResponseDto(200, "OK");
@@ -68,11 +80,13 @@ public class TaskController {
     @DeleteMapping("/{projectId}/{taskId}")
     public MessageResponseDto deleteTask(
             @PathVariable String projectId,
-            @PathVariable String taskId){
+            @PathVariable String taskId,
+            @RequestHeader(value = "X-USER-ID", required = true) String userId){
 
         long projectIdLong = parseId(projectId);
         long taskIdLong = parseId(taskId);
 
+        commonService.isUserMemberOfProject(projectIdLong, userId);
         taskService.deleteTask(projectIdLong, taskIdLong);
 
         return new MessageResponseDto(200, "OK");
